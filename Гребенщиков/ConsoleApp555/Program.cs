3c689349d7306f5f48c85db6bb01fa6626ef3c70
DanilG73 <123488618+DanilG73@users.noreply.github.com>
2023-11-29 15:29:36 +0200

    Add files via upload

---
+++ Гребенщиков/ConsoleApp555/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp555
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер задачи: ");
+            int po = int.Parse(Console.ReadLine());
+            switch (po)
+            {
+                case 1:
+                    Console.Write("Введите количество дней: ");
+            int n = int.Parse(Console.ReadLine());
+
+            DateTime date = new DateTime(2000, 1, 1).AddDays(n);
+
+            Console.WriteLine($"Текущий месяц: {date.ToString("MMMM")}");
+            break;
+                case 2:
+                    int[,] goals = new int[11, 3] { { 1, 2,0 }, { 0, 0, 0 },
+                    { 2, 0, 0 }, { 1, 0, 1 }, { 0, 3, 0}, { 0, 1, 0 },
+                    { 3, 0, 0 }, { 2, 2, 0}, {0, 2, 0 }, { 4, 0, 2 }, {0 , 0, 1 } };
+
+                    int rows = goals.GetUpperBound(0) + 1;
+                    int columns = goals.Length / rows;
+                    int count = 0;
+                    int goalSum = 0;
+
+                    for (int i = 0; i < rows; i++)
+                    {
+                        goalSum = 0;
+                        for (int j = 0; j < columns; j++)
+                        {
+                            goalSum += goals[i, j];
+
+                        }
+
+                        if (goalSum >= 2)
+                        {
+                            count++;
+                        }
+                    }
+
+                    Console.WriteLine($"Число футблоистов, забивших хотя бы два гола: {count}");
+                    break;
+        }
+        Console.ReadKey();
+                                
+        }
+    }
+}
---
+++ Гребенщиков/ConsoleApp666/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp666
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            again:
+            Console.Write("Введте номер задачи: ");
+            int pr = int.Parse(Console.ReadLine());
+            switch (pr)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Во время сильного дождя на остановке стояло 12 человек. Подкативший автобус забрызгал грязью 4-го, 5-го, 6, 7 и 8-го человека. Остальные попрыгали в колючие кусты, причем 3-ий, 9-ый и 12-ый так и не смогли выбраться из них. Задать номер пассажира и определить грязный он или исцарапанный.");
+                        Console.WriteLine("На остановке стоит 12 человек");
+                        int a = int.Parse(Console.ReadLine());
+                        if (a >= 4 && a <= 8)
+                        {
+                            Console.WriteLine("едет в автобусе ,и он грязный ");
+                        }
+                        else if (a == 3 || a == 9 || a == 12)
+                        {
+                            Console.WriteLine("он в кустах");
+                        }
+                        else if (a == 1 || a == 2 || a == 10 || a == 11)
+                        {
+                            Console.WriteLine("едет в автобусе и поцарапаный");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Данного пасажира не существует на остановке.ВВЕДИТЕ ИМЕННО ЧИСЛО ОТ 1 ДО 12!");
+                        }
+                        goto again;
+                    }
+
+                    
+                case 2:
+                    {
+                        Console.WriteLine("В трехзначном числе х зачеркнули его вторую цифру. Когда к образованному при этом двузначному числу справа приписали вторую цифру числа х, то получилось число 456. Найти число х");
+                        Console.Write("Введите трехзначное число: ");
+                        int x = int.Parse(Console.ReadLine());
+                        int a1 = x / 100;
+                        int b = x / 10 % 10;
+                        int d = x % 10;
+                        x = a1 * 100 + d * 10 + b;
+                        Console.Write($"{x}");
+                        goto again;
+                    }
+            }
+        }
+        
+        
+        
+    }
+}
